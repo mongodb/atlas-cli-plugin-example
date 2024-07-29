52dcbd5279f331f5f8da486fc165c2261a2dfaeb
Stefan Humpelstetter <stefan.humpelstetter@mongodb.com>
2024-07-29 17:35:13 +0100

    comment

--- main.go
+++ main.go
@@ -22,31 +22,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func EchoBuilder() *cobra.Command {
-	return &cobra.Command{
-		Use: "echo",
-		Short: "Echos the input args",
-		RunE: func(_ *cobra.Command, args []string) error {
-			if len(args) == 0 {
-				return errors.New("no args given")
-			}
-			for _, arg := range args {
-				fmt.Printf("%s ", arg)
-			}
-			return nil
-		},
-	}
-}
+// func EchoBuilder() *cobra.Command {
+// 	return &cobra.Command{
+// 		Use: "echo",
+// 		Short: "Echos the input args",
+// 		RunE: func(_ *cobra.Command, args []string) error {
+// 			if len(args) == 0 {
+// 				return errors.New("no args given")
+// 			}
+// 			for _, arg := range args {
+// 				fmt.Printf("%s ", arg)
+// 			}
+// 			return nil
+// 		},
+// 	}
+// }
 
-func HelloBuilder() *cobra.Command {
-	return &cobra.Command{
-		Use: "hello",
-		Short: "The Hello World command",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello World!")
-		},
-	}
-}
+// func HelloBuilder() *cobra.Command {
+// 	return &cobra.Command{
+// 		Use: "hello",
+// 		Short: "The Hello World command",
+// 		Run: func(cmd *cobra.Command, args []string) {
+// 			fmt.Println("Hello World!")
+// 		},
+// 	}
+// }
 
 func main() {
 	var rootCmd = &cobra.Command{
@@ -54,10 +54,10 @@ func main() {
 		Short: "Root command of the atlas cli plugin example",
 	}
 
-	rootCmd.AddCommand(
-		HelloBuilder(),
-		EchoBuilder(),
-	)
+	// rootCmd.AddCommand(
+	// 	HelloBuilder(),
+	// 	EchoBuilder(),
+	// )
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
