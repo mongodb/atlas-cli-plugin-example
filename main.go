06d83080031f10d6898e3bfa47cc46747acb6d3c
Stefan Humpelstetter <stefan.humpelstetter@mongodb.com>
2024-07-30 10:43:34 +0100

    test

--- main.go
+++ main.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -22,6 +23,31 @@ import (
 )
 
 
+func HelloBuilder() *cobra.Command {
+	return &cobra.Command{
+		Use: "hello",
+		Short: "The Hello World command",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello World!")
+		},
+	}
+}
+
+func EchoBuilder() *cobra.Command {
+	return &cobra.Command{
+		Use: "echo",
+		Short: "Echos the input args",
+		RunE: func(_ *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return errors.New("no args given")
+			}
+			for _, arg := range args {
+				fmt.Printf("%s ", arg)
+			}
+			return nil
+		},
+	}
+}
 
 func main() {
 	var rootCmd = &cobra.Command{
